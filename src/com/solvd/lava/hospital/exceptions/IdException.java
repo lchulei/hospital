ad3f917dca9c6f3ec7fa84d258eeaf7022d6f459
Liliia Chulei <liliia.chulei@gmail.com>
2022-08-12 16:18:47 +0300

    write exception

---
+++ src/com/solvd/lava/hospital/exceptions/IdException.java
@@ -0,0 +1,35 @@
+package com.solvd.lava.hospital.exceptions;
+
+import java.io.IOException;
+
+public class IdException extends IOException {
+
+    private int id;
+
+    public IdException(String message) {
+
+        super(message);
+
+    }
+
+    public IdException(String message, int id) {
+
+        super(message);
+
+        this.id = id;
+
+    }
+
+    public int getId() {
+
+        return id;
+
+    }
+
+    public void setId(int id) {
+
+        this.id = id;
+
+    }
+
+}
